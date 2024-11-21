9130625eff36b48b4c3c0f9a2ffa4d3fbea2df09
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-11-21 12:05:49 +0100

    feat: add :host css rules + dynamic classes

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,7 +7,21 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'
+  // @HostListener('click') onClick() {
+  //   console.log('clicked')
+  // }
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('clicked');
+    console.log(this.el);
+  }
 }
