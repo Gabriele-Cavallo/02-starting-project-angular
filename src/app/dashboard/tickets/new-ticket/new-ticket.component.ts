3dc03b8bbc2e5922d5f6155c1414c8764f4dee72
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-11-21 15:58:06 +0100

    feat: add ngOnInit + ngAfterViewInit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
@@ -11,9 +11,19 @@ import { ControlComponent } from "../../../shared/control/control.component";
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
   // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   private form = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit() {
+    console.log('oninit');
+    console.log(this.form()?.nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('after view init');
+    console.log(this.form()?.nativeElement);
+  }
 
   onSubmit(titleElement: string, ticketText: string) {
     const enteredTitle = titleElement;
