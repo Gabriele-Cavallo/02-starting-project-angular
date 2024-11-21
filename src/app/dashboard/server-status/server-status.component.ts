cf58f6b6b16885976f8fa5821cec794e8b72376a
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-11-21 16:41:07 +0100

    feat: add signals + effect in ServerStatusComponent

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, Input, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,11 +8,15 @@ import { Component, DestroyRef, inject, Input, OnDestroy, OnInit } from '@angula
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus  = signal<'online' | 'offline' | 'unknown'>('offline');
   // private interval?: NodeJS.Timeout;
   private destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus());
+    });
+  }
 
   ngOnInit() {
     console.log('on init');
@@ -20,11 +24,11 @@ export class ServerStatusComponent implements OnInit {
       const rnd = Math.random();
 
       if(rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
 
